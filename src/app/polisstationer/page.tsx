8d708bd5020a934ee581b1449f6335f17b081f83
KIFLI <linus.h.frick@gmail.com>
2023-07-07 00:15:30 +0200

    dynamic routing

--- src/app/polisstationer/page.tsx
+++ src/app/polisstationer/page.tsx
@@ -1,4 +1,5 @@
 import { Stations } from "@/models/stations";
+import Link from 'next/link';
 
 async function getStations(){
     const res = await fetch('https://polisen.se/api/policestations');
@@ -12,14 +13,17 @@ async function getStations(){
 export default async function Page(){
     const stations: Stations = await getStations();
 
+
     return (
         <div className="d-flex flex-column align-items-center">
             <div>polisstationer</div>
             <div className="flex flex-col gap-6">
         {stations.map((item: Stations) => (
         <div key={item.id} className="border">
-          <h1>{item.name}</h1>
-          <p>{item.summary}</p>
+            <Link href={`/polisstationer/${item.id}`}>
+                <h1>{item.name}</h1>
+            </Link>
+          <p>{item.location.name}</p>
         </div>
       ))}
     </div>
